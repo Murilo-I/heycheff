44829b5551dde6e56011441643f25fea68053122
murilo.tegani <murilo.tegani@superbid.net>
2022-12-04 21:41:23 -0300

    tela inicial e feed

---
+++ feed.js
@@ -0,0 +1,15 @@
+timeline = document.querySelector('.timeline');
+
+fetch('data.json').then(resp => resp.json()).then(json => {
+    for (var i = 0; i < json.length; i++) {
+        var div = document.createElement('div');
+        var img = document.createElement('img');
+        var h3 = document.createElement('h3');
+        h3.innerText = json[i].title;
+        img.src = json[i].path;
+        div.appendChild(img);
+        div.appendChild(h3);
+        div.classList.add('thumb');
+        timeline.appendChild(div);
+    }
+});
