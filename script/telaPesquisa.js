83553d6c83197f789a75b706852c2bcfaf29a66e
murilo.tegani <murilo.tegani@superbid.net>
2022-12-07 23:15:26 -0300

    resolvendo images path

--- script/telaPesquisa.js
+++ script/telaPesquisa.js
@@ -12,31 +12,31 @@ function loadJapa() {
 }
 
 function loadChurras() {
-    imgCard.src = "../images/churras.jpg";
+    imgCard.src = "images/churras.jpg";
     txtCard.innerHTML = "Faça um churrasco que todo brasileiro respeite";
 }
 
 function loadDoce() {
-    imgCard.src = "../images/doce.jpg";
+    imgCard.src = "images/doce.jpg";
     txtCard.innerHTML = "Torta de morango caseira, ninguém supera";
 }
 
 function loadVeg() {
-    imgCard.src = "../images/veg.jpg";
+    imgCard.src = "images/veg.jpg";
     txtCard.innerHTML = "Hamburguer vegano, poupe as vaquinhas";
 }
 
 function loadFit() {
-    imgCard.src = "../images/fit.jpg";
+    imgCard.src = "images/fit.jpg";
     txtCard.innerHTML = "Entre na dieta com Frango Fit";
 }
 
 function loadMex() {
-    imgCard.src = "../images/mex.jpg";
+    imgCard.src = "images/mex.jpg";
     txtCard.innerHTML = "O burrito que todo mexicano adora";
 }
 
 function loadArab() {
-    imgCard.src = "../images/arab.jpg";
+    imgCard.src = "images/arab.jpg";
     txtCard.innerHTML = "Comida Árabe não é só esfirra e kibbe";
 }
