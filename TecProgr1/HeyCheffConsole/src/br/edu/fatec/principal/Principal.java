f40ab5aedf58d5a9dca76c8d3ea7f90a0025e766
brunoguez <brunomguez@gmail.com>
2023-03-29 15:41:40 -0300

    Criação do projeto HeyCheffConsole

---
+++ TecProgr1/HeyCheffConsole/src/br/edu/fatec/principal/Principal.java
@@ -0,0 +1,9 @@
+package br.edu.fatec.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ TecProgr1/HeyCheffConsole/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Bruno
+ *
+ */
+module HeyCheffConsole {
+}
