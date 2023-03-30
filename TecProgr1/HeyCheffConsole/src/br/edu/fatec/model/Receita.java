8f76fbbaf6066039bcbd0c07002ecc8ac82a9d69
brunoguez <91354018+brunoguez@users.noreply.github.com>
2023-03-29 23:30:57 -0300

    Atualizar o Receita.java

--- TecProgr1/HeyCheffConsole/src/br/edu/fatec/model/Receita.java
+++ TecProgr1/HeyCheffConsole/src/br/edu/fatec/model/Receita.java
@@ -1,5 +1,12 @@
 package br.edu.fatec.model;
 
 public class Receita {
-	
+	private int id;
+    private String descricao;
+    private Step[] steps;
+    private int qtReceitas;
+    
+    public Receita(){
+        
+    } 
 }
